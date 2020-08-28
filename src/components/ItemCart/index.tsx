b12c2a6df5c663c2750e8b819864d851d2ac508b
Josias Furtado <jo.junior94@gmail.com>
2020-08-28 13:52:20 -0300

    add item cart

---
+++ src/components/ItemCart/index.tsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react'
+import {
+  StyleProp,
+  Text,
+  View,
+  ViewStyle,
+  TouchableOpacity,
+  Image,
+  TouchableHighlight,
+} from 'react-native'
+import { tailwind, getColor } from 'lib/styles'
+import { FontAwesome5, Ionicons } from '@expo/vector-icons'
+
+interface ItemCartProps {
+  readonly style?: StyleProp<ViewStyle>
+  readonly itemPrice: number
+  setQuantityMock: any
+  quantityMock: number
+}
+
+const ItemCart: React.FC<ItemCartProps> = ({
+  style,
+  itemPrice,
+  setQuantityMock,
+  quantityMock,
+}) => {
+  const itemPriceMultipliedByQuantity = parseFloat(
+    (itemPrice * quantityMock).toFixed(2),
+  )
+  const itemPriceMultipliedWithComma = String(
+    itemPriceMultipliedByQuantity,
+  ).replace('.', ',')
+
+  const handleIncreasesItemQuantity = () => {
+    setQuantityMock(quantityMock + 1)
+  }
+
+  const handleDecreasesItemQuantity = () => {
+    if (quantityMock === 1) {
+      return
+    }
+    setQuantityMock(quantityMock - 1)
+  }
+
+  return (
+    <View style={tailwind('w-full mb-2')}>
+      <View
+        style={tailwind(
+          'flex-row justify-between border-b pb-3 border-gray-500',
+        )}
+      >
+        <View style={tailwind('flex-row items-center w-1/2')}>
+          <TouchableOpacity style={tailwind('h-6 w-6')}>
+            <FontAwesome5 name="trash" size={20} color={getColor('gray-500')} />
+          </TouchableOpacity>
+          <Image
+            resizeMode="cover"
+            style={[
+              tailwind('rounded h-10 w-10 ml-2'),
+              { borderColor: '#c4c4c4', borderWidth: 1 },
+            ]}
+            source={{
+              uri: 'https://belezaesaude.com/i/730/56/tomate.jpg',
+            }}
+          />
+          <View style={tailwind('ml-2 w-3/5')}>
+            <Text
+              lineBreakMode="tail"
+              numberOfLines={1}
+              style={tailwind('text-base font-bold')}
+            >
+              Tomates 1kg dasdsdadasd adssadasdsa sdadas
+            </Text>
+            <Text
+              lineBreakMode="tail"
+              numberOfLines={1}
+              style={tailwind('text-base')}
+            >
+              R$ 11,99
+            </Text>
+            <TouchableOpacity style={tailwind('flex-row items-center')}>
+              <FontAwesome5
+                name="edit"
+                size={12}
+                color={getColor('gray-600')}
+              />
+              <Text
+                lineBreakMode="tail"
+                numberOfLines={1}
+                style={tailwind('text-xs text-gray-600')}
+              >
+                observações
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={tailwind('flex-row items-center')}>
+          <TouchableHighlight
+            underlayColor={getColor('gray-200')}
+            onPress={handleDecreasesItemQuantity}
+            style={tailwind(
+              `bg-white border-2 w-8 h-8 justify-center items-center rounded ${
+                quantityMock > 1 ? 'border-primary-500' : 'border-gray-500'
+              }`,
+            )}
+          >
+            <Ionicons
+              name="ios-remove"
+              size={24}
+              color={getColor(
+                `${quantityMock > 1 ? 'primary-500' : 'gray-500'}`,
+              )}
+            />
+          </TouchableHighlight>
+          <Text style={tailwind('text-2xl bg-gray-200 font-medium px-2')}>
+            {quantityMock}
+          </Text>
+          <TouchableHighlight
+            underlayColor={getColor('gray-200')}
+            onPress={handleIncreasesItemQuantity}
+            style={tailwind(
+              'bg-white border-2 border-primary-500 w-8 h-8 justify-center items-center rounded',
+            )}
+          >
+            <Ionicons
+              name="ios-add"
+              size={24}
+              color={getColor('primary-500')}
+            />
+          </TouchableHighlight>
+        </View>
+        <View style={tailwind('justify-center')}>
+          <Text style={tailwind('text-lg')}>
+            {itemPriceMultipliedWithComma}
+          </Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+ItemCart.displayName = 'ItemCart'
+
+export default ItemCart
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, SafeAreaView, ScrollView, Image } from 'react-native'
-import { tailwind } from 'lib/styles'
+import { tailwind, getColor } from 'lib/styles'
 import logo from '../../../assets/png/logo-without-text.png'
+import { FontAwesome5, Ionicons } from '@expo/vector-icons'
+import PrimaryButton from 'components/styledComponents/PrimaryButton'
+import ItemCart from 'components/ItemCart'
 
 const Cart: React.FC = () => {
+  const [quantityMock, setQuantityMock] = useState(1)
+  const itemPrice = 11.99
+  const itemPriceMultipliedByQuantity = parseFloat(
+    (itemPrice * quantityMock).toFixed(2),
+  )
+
+  const totalmock = itemPriceMultipliedByQuantity + 4
+  const totalmockstring = String(totalmock).replace('.', ',')
+
   return (
     <SafeAreaView style={tailwind('flex-1 relative bg-white')}>
       <ScrollView>
@@ -15,31 +27,51 @@ const Cart: React.FC = () => {
           />
         </View>
         <View style={tailwind('-mt-4 rounded-t-xl bg-white px-4')}>
-          <Text style={tailwind('text-lg text-primary-500 mb-2 pt-4')}>
+          <Text
+            style={tailwind('text-lg text-primary-500 mb-2 pt-4 font-bold')}
+          >
             Seu carrinho
           </Text>
           <View style={tailwind('flex-row justify-between mb-4')}>
-            <Text style={tailwind('text-lg')}>Item</Text>
-            <Text style={tailwind('text-lg')}>Subtotal</Text>
+            <Text style={tailwind('text-lg font-bold')}>Item</Text>
+            <Text style={tailwind('text-lg font-bold')}>Subtotal</Text>
           </View>
-          <View>
-            <View style={tailwind('flex-row')}>
-              <Image
-                resizeMode="cover"
-                style={[
-                  tailwind('rounded h-16 w-16'),
-                  { borderColor: '#c4c4c4', borderWidth: 1 },
-                ]}
-                source={{ uri: 'https://belezaesaude.com/i/730/56/tomate.jpg' }}
-              />
-              <View style={tailwind('ml-2')}>
-                <Text style={tailwind('text-base font-bold')}>Tomates 1kg</Text>
-                <Text style={tailwind('text-base')}>R$ 11,99</Text>
-              </View>
+          <ItemCart
+            itemPrice={itemPrice}
+            quantityMock={quantityMock}
+            setQuantityMock={setQuantityMock}
+          />
+          <View style={tailwind('border-b pb-3 border-gray-500 mb-4')}>
+            <View style={tailwind('flex-row justify-between mb-1')}>
+              <Text style={tailwind('text-lg font-bold')}>Taxa de entrega</Text>
+              <Text style={tailwind('text-lg font-bold text-primary-500')}>
+                Total
+              </Text>
+            </View>
+            <View style={tailwind('flex-row justify-between items-end')}>
+              <Text style={tailwind('text-lg')}>+ R$ 4,00</Text>
+              <Text style={tailwind('text-2xl')}>{totalmockstring}</Text>
             </View>
           </View>
         </View>
+        <View style={tailwind('px-8 pb-4')}>
+          <PrimaryButton
+            onPress={() => {}}
+            style={tailwind('flex-row bg-gray-100 border-2 border-gray-300')}
+          >
+            <Text style={tailwind('mr-2 text-lg text-gray-600')}>
+              ESVAZIAR CARRINHO
+            </Text>
+            <FontAwesome5 name="trash" size={18} color={getColor('gray-500')} />
+          </PrimaryButton>
+        </View>
       </ScrollView>
+      <View style={tailwind('px-8 pb-4')}>
+        <PrimaryButton onPress={() => {}} style={tailwind('flex-row')}>
+          <Text style={tailwind('mr-2 text-lg text-white')}>CONFIRMAR</Text>
+          <Ionicons name="md-checkmark" size={20} color="#fff" />
+        </PrimaryButton>
+      </View>
     </SafeAreaView>
   )
 }
